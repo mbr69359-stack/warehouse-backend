cbba29c67d5deecd879ebda669f3ad18ed61e795
mbr69359-stack <mbr69359@gmail.com>
2026-06-09 23:05:18 +0800

    fix: escape reserved keywords key/value in SysConfig entity

--- src/main/java/com/warehouse/entity/SysConfig.java
+++ src/main/java/com/warehouse/entity/SysConfig.java
@@ -1,5 +1,6 @@
 package com.warehouse.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
@@ -7,8 +8,9 @@ import lombok.Data;
 @Data
 @TableName("sys_config")
 public class SysConfig {
-    @TableId
+    @TableId("`key`")
     private String key;
+    @TableField("`value`")
     private String value;
     private String remark;
 }
